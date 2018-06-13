7c1eb0634758ae442f0b3c4dd58cb31a6231cfdf
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-06-13 14:11:51 +0200

    Adds actul error cases

--- spec/poe/sniper/known_issues/clipboard_spec.rb
+++ spec/poe/sniper/known_issues/clipboard_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 RSpec.describe "cliboard" do
   it "can place ; to cliboard" do
     skip unless Gem.win_platform?
-    expect { Clipboard.set_data("test;", format = Clipboard::UNICODETEXT) }.not_to raise_error
+    text = "@Rabuarc Hi, I would like to buy your The Hoarder listed for 10 chaos in Incursion (stash tab D; position: left 19, top 1)"
+    expect { Clipboard.set_data(text, format = Clipboard::UNICODETEXT) }.not_to raise_error
+    text2 = "@WanderDay Hi, I would like to buy your The Hoarder listed for 10 chaos in Incursion (stash tab $; position: left 6, top 4)"
+    expect { Clipboard.set_data(text2, format = Clipboard::UNICODETEXT) }.not_to raise_error
   end
 end
