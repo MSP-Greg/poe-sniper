20986266b80de038a22e34839a7331483f7d0645
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-06-13 13:52:43 +0200

    Fixes testcase

--- spec/poe/sniper/known_issues/clipboard_spec.rb
+++ spec/poe/sniper/known_issues/clipboard_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 RSpec.describe "cliboard" do
   it "can place ; to cliboard" do
     skip unless Gem.win_platform?
-    expect(Clipboard.set_data("test;", format = Clipboard::UNICODETEXT)).not_to raise_error
+    expect { Clipboard.set_data("test;", format = Clipboard::UNICODETEXT) }.not_to raise_error
   end
 end
