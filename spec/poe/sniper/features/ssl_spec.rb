2423630fd59dcf08487a279ccd93c5a2bd4d420e
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-06-13 11:57:51 +0200

    Adds test for EM.ssl? to be true

--- spec/poe/sniper/features/ssl_spec.rb
+++ spec/poe/sniper/features/ssl_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 RSpec.describe "ssl support" do
+  it "EM.ssl? returns true" do
+    expect(EM.ssl?).to be_truthy
+  end
+
   it "can connect to wss" do
     EM.run {
       ws = Faye::WebSocket::Client.new('wss://echo.websocket.org')
