42027b1200bb0b95cf6c02941afb44e92c58b502
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-06-13 13:39:07 +0200

    Adds clipboard spec

---
+++ spec/poe/sniper/known_issues/clipboard_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+RSpec.describe "cliboard" do
+  it "can place ; to cliboard" do
+    skip unless Gem.win_platform?
+    expect(Clipboard.set_data("test;", format = Clipboard::UNICODETEXT)).not_to raise_error
+  end
+end
